9b75c7943dddc3ee3767d1e7a7ab9f11f87976f1
besaklish <34913036+besaklish@users.noreply.github.com>
2024-08-31 03:10:36 +0000

    added random projects

---
+++ src/kangaroo/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+class Person 
+{
+    public string Name;
+    public int Age;
+    public static string ScientificName;
+}
+
+var p = new Person { Name: "" }
+
